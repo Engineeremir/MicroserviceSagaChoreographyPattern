0cf56237d77178196e367b19faded79e0d3de817
Emirhan KALEM <emirhan.kalem320@gmail.com>
2023-03-14 12:12:46 +0300

    Add Address dto to OrderCreateDto

--- MicroserviceSagaPattern.Order.API/Dtos/OrderCreateDto.cs
+++ MicroserviceSagaPattern.Order.API/Dtos/OrderCreateDto.cs
@@ -5,5 +5,6 @@
         public string BuyerId { get; set; }
         public List<OrderItemDto> OrderItems { get; set; }
         public PaymentDto Payment { get; set; }
+        public AddressDto Address{ get; set; }
     }
 }
