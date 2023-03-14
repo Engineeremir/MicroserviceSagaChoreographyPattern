1ba9947edaf38b028e2872c45c72738d96682055
Emirhan KALEM <emirhan.kalem320@gmail.com>
2023-03-14 12:10:54 +0300

    Create Dto's for OrderCreate service

---
+++ MicroserviceSagaPattern.Order.API/Dtos/OrderCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MicroserviceSagaPattern.Order.API.Dtos
+{
+    public class OrderCreateDto
+    {
+        public string BuyerId { get; set; }
+        public List<OrderItemDto> OrderItems { get; set; }
+        public PaymentDto Payment { get; set; }
+    }
+}
---
+++ MicroserviceSagaPattern.Order.API/Dtos/PaymentDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MicroserviceSagaPattern.Order.API.Dtos
+{
+    public class PaymentDto
+    {
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string Expiration { get; set; }
+        public string CVV { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
