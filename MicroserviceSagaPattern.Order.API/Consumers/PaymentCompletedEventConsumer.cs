8a8c952272911e3194258592459e63468761fb39
Emirhan KALEM <emirhan.kalem320@gmail.com>
2023-03-14 16:39:23 +0300

    Payment Completed Event

--- MicroserviceSagaPattern.Order.API/Consumers/PaymentCompletedEventConsumer.cs
+++ MicroserviceSagaPattern.Order.API/Consumers/PaymentCompletedEventConsumer.cs
@@ -1,15 +1,38 @@
 ﻿using MassTransit;
+using MicroserviceSagaPattern.Order.API.Models;
 using MicroserviceSagaPattern.Shared.Events.Payment;
 
 namespace MicroserviceSagaPattern.Order.API.Consumers
 {
     public class PaymentCompletedEventConsumer : IConsumer<PaymentCompletedEvent>
     {
+        private readonly ApplicationDbContext _context;
         private readonly ISendEndpointProvider _sendEndpointProvider;
         private readonly IPublishEndpoint _publishEndpoint;
-        public Task Consume(ConsumeContext<PaymentCompletedEvent> context)
+        private readonly ILogger<PaymentCompletedEvent> _logger;
+
+        public PaymentCompletedEventConsumer(ApplicationDbContext context, ISendEndpointProvider sendEndpointProvider, IPublishEndpoint publishEndpoint, ILogger<PaymentCompletedEvent> logger)
         {
-            throw new NotImplementedException();
+            _context = context;
+            _sendEndpointProvider = sendEndpointProvider;
+            _publishEndpoint = publishEndpoint;
+            _logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<PaymentCompletedEvent> context)
+        {
+            var order = await _context.Orders.FindAsync(context.Message.OrderId);
+            if (order != null)
+            {
+                order.OrderStatus = OrderStatus.Completed;
+                await _context.SaveChangesAsync();
+                _logger.LogInformation($"Order (Id={context.Message.OrderId}) status changed : {order.OrderStatus}");
+
+            }
+            else
+            {
+                _logger.LogInformation($"Order (Id={context.Message.OrderId}) not found");
+            }
         }
     }
 }
--- MicroserviceSagaPattern.Order.API/Models/Order.cs
+++ MicroserviceSagaPattern.Order.API/Models/Order.cs
@@ -13,7 +13,7 @@
     public enum OrderStatus
     {
         Suspend,
-        Success,
+        Completed,
         Fail
     }
 }
