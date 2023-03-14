c61d301b8d49ff5c794c40c94cb02eec9afc5097
Emirhan KALEM <emirhan.kalem320@gmail.com>
2023-03-14 14:12:27 +0300

    Stock Migration

---
+++ MicroserviceSagaPattern.Stock.API/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MicroserviceSagaPattern.Stock.API.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Stock> Stocks{ get; set; }
+
+
+
+    }
+}
--- MicroserviceSagaPattern.Stock.API/Program.cs
+++ MicroserviceSagaPattern.Stock.API/Program.cs
@@ -1,3 +1,6 @@
+using MicroserviceSagaPattern.Stock.API.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"));
+});
+
+
 
 var app = builder.Build();
 
